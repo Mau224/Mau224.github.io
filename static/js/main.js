9b10ce8a4dfa08b6b0074b0777b64f916538a374
Mau224 <46903680+Mau224@users.noreply.github.com>
2020-08-08 15:33:43 +0300

    велы

---
+++ static/js/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu,.header__body').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+$(document).ready(function () {
+  $('.header__link').click(function () {
+    $('.header__burger,.header__menu,.header__body').removeClass('active');
+    $('body').removeClass('lock');
+  });
+});
+$('.header-content__input--name').on('input', function () {
+  $(this).val($(this).val().replace(/^[ 0-9]+$/, ''));
+});
+$(function () {
+  $('#tel').mask('+7 (999)–999–99-99');
+});
+"use strict";
+
+if (window.localStorage) {
+  var elements = document.querySelectorAll('[name]');
+
+  for (var i = 0, length = elements.length; i < length; i++) {
+    (function (element) {
+      var name = element.getAttribute('name');
+      element.value = localStorage.getItem(name) || '';
+
+      element.onkeyup = function () {
+        localStorage.setItem(name, element.value);
+      };
+    })(elements[i]);
+  }
+}
