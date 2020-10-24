7463ae6f41a5c9dd855b7c189b154f974e0b64a1
Mau224 <46903680+Mau224@users.noreply.github.com>
2020-10-24 16:43:33 +0300

    form page

--- blog-single/js/main.js
+++ blog-single/js/main.js
@@ -132,4 +132,75 @@
 }());
 
 'use strict';
+let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.select'),
+            currentText = select.querySelector('.select__current');
+        currentText.innerText = text;
+        $('input[type="hidden"]').val(text);
+        select.classList.remove('is-active');
+
+    }
+
+};
+
+
+select();
+
+// Select
+$('.slct').click(function(){
+    /* Заносим выпадающий список в переменную */
+    var dropBlock = $(this).parent().find('.drop');
+
+    /* Делаем проверку: Если выпадающий блок скрыт то делаем его видимым*/
+    if( dropBlock.is(':hidden') ) {
+        dropBlock.slideDown();
+
+        /* Выделяем ссылку открывающую select */
+        $(this).addClass('active');
+
+        /* Работаем с событием клика по элементам выпадающего списка */
+        $('.drop').find('li').click(function(){
+
+            /* Заносим в переменную HTML код элемента
+            списка по которому кликнули */
+            var selectResult = $(this).html();
+
+            /* Находим наш скрытый инпут и передаем в него
+            значение из переменной selectResult */
+            $(this).parent().parent().find('input').val(selectResult);
+
+            /* Передаем значение переменной selectResult в ссылку которая
+            открывает наш выпадающий список и удаляем активность */
+            $(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+            /* Скрываем выпадающий блок */
+            dropBlock.slideUp();
+        });
+
+        /* Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его */
+    } else {
+        $(this).removeClass('active');
+        dropBlock.slideUp();
+    }
+
+    /* Предотвращаем обычное поведение ссылки при клике */
+    return false;
+});
 
