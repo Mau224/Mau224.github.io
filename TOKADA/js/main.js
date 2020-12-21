1f290c938688b04ce77936493a4ca3afa8b534c8
Mau224 <46903680+Mau224@users.noreply.github.com>
2020-12-21 09:13:33 +0300

    big path

--- TOKADA/js/main.js
+++ TOKADA/js/main.js
@@ -278,6 +278,70 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.remove('dis');
     });
 }());
+const swiper = new Swiper('.recomend__slider', {
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // centeredSlides: true,
+    // clickable: true,
+    spaceBetween: 14,
+    // loop: true,
+    // updateOnWindowResize: true,
+
+    slidesPerView: 1.34,
+    slidesOffsetBefore: 20,
+    slidesOffsetAfter: 20,
+
+    breakpoints: {
+    867: {
+        slidesPerView: 3.1,
+        loop: true,
+        spaceBetween: 40,
+        // slidesOffsetBefore: 100,
+        // slidesOffsetAfter: 100,
+    },
+    }
+});
+
+const slide = document.querySelector('.recomend__item')
+
+swiper.on('slideChange', function () {
+    slide.classList.remove('recomend__item--first');
+});
+
+// const slider = document.querySelector('.whyus__wrapper');
+const sliders = document.querySelectorAll('.slider');
+
+function mobileSlider() {
+    sliders.forEach((el) => {
+        let swiperMob = new Swiper(el, {
+            slidesPerView: 1.24,
+            spaceBetween: 22,
+            navigation: {
+                nextEl: el.querySelector('.swiper-button-next'),
+                prevEl: el.querySelector('.swiper-button-prev'),
+            },
+    });
+
+
+        if (window.innerWidth > 867) {
+            el.dataset.mobile = 'false';
+            if (el.classList.contains('swiper-container-initialized')) {
+                swiperMob.destroy();
+            }
+        }
+    });
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider();
+});
+
+
 // let spoiler = document.querySelectorAll('.faq__item-header');
 
 // for (let i = 0; i < spoiler.length; i++) {
@@ -315,3 +379,107 @@ for (let i = 0; i < checkbox.length; i++) {
     })
 }
 
+
+document.addEventListener('DOMContentLoaded', () => {
+	const newYear = new Date('Dec 28 2021 00:00:00');
+
+	// const daysVal = document.querySelector('.time-count__days .time-count__val');
+	const hoursVal = document.querySelector('.time-count__hours .time-count__val');
+	const minutesVal = document.querySelector('.time-count__minutes .time-count__val');
+	const secondsVal = document.querySelector('.time-count__seconds .time-count__val');
+
+	// const daysText = document.querySelector('.time-count__days .time-count__text');
+	const hoursText = document.querySelector('.time-count__hours .time-count__text');
+	const minutesText = document.querySelector('.time-count__minutes .time-count__text');
+	const secondsText = document.querySelector('.time-count__seconds .time-count__text');
+
+	function declOfNum(number, titles) {
+		let cases = [2, 0, 1, 1, 1, 2];
+		return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+	}
+
+	const timeCount = () => {
+		let now = new Date();
+		let leftUntil = newYear - now;
+
+		// let days = Math.floor(leftUntil / 1000 / 60 / 60 / 24);
+		let hours = Math.floor(leftUntil / 1000 / 60 / 60) % 24;
+		let minutes = Math.floor(leftUntil / 1000 / 60) % 60;
+		let seconds = Math.floor(leftUntil / 1000) % 60;
+
+		if (hours < 10) {
+			hoursVal.textContent = '0' + hours;
+			hoursVal.style.letterSpacing = '7.5vw';
+		} else if (hours < 20) {
+			hoursVal.textContent = hours;
+			hoursVal.style.letterSpacing = '8.7vw';
+		} else {
+			hoursVal.textContent = hours;
+			hoursVal.style.letterSpacing = '7.5vw';
+		}
+
+		if (minutes < 10) {
+			minutesVal.textContent = '0' + minutes;
+			minutesVal.style.letterSpacing = '7.8vw';
+		} else if (minutes < 20) {
+			minutesVal.textContent = minutes;
+			minutesVal.style.letterSpacing = '8.7vw';
+		} else {
+			minutesVal.textContent = minutes;
+			minutesVal.style.letterSpacing = '7.8vw';
+		}
+
+		if (seconds < 10) {
+			secondsVal.textContent = '0' + seconds;
+			secondsVal.style.letterSpacing = '7.65vw';
+		} else if (seconds < 20) {
+			secondsVal.textContent = seconds;
+			secondsVal.style.letterSpacing = '8.7vw';
+		} else {
+			secondsVal.textContent = seconds;
+			secondsVal.style.letterSpacing = '7.65vw';
+		}
+
+		if (window.innerWidth < 870) {
+			if (hours < 10) {
+				hoursVal.textContent = '0' + hours;
+				hoursVal.style.letterSpacing = '57px';
+			} else if (hours < 20) {
+				hoursVal.textContent = hours;
+				hoursVal.style.letterSpacing = '65px';
+			} else {
+				hoursVal.textContent = hours;
+				hoursVal.style.letterSpacing = '57px';
+			}
+
+			if (minutes < 10) {
+				minutesVal.textContent = '0' + minutes;
+				minutesVal.style.letterSpacing = '57px';
+			} else if (minutes < 20) {
+				minutesVal.textContent = minutes;
+				minutesVal.style.letterSpacing = '65px';
+			} else {
+				minutesVal.textContent = minutes;
+				minutesVal.style.letterSpacing = '57px';
+			}
+
+			if (seconds < 10) {
+				secondsVal.textContent = '0' + seconds;
+				secondsVal.style.letterSpacing = '57px';
+			} else if (seconds < 20) {
+				secondsVal.textContent = seconds;
+				secondsVal.style.letterSpacing = '65px';
+			} else {
+				secondsVal.textContent = seconds;
+				secondsVal.style.letterSpacing = '57px';
+			}
+		}
+
+		hoursText.textContent = declOfNum(hours, ['час', 'часа', 'часов']);
+		minutesText.textContent = declOfNum(minutes, ['минута', 'минуты', 'минут']);
+		secondsText.textContent = declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
+	};
+
+	timeCount();
+	setInterval(timeCount, 1000);
+});
