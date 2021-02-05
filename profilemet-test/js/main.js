2ad87e64bc5d400dfdb045a8b680457ec43ac35a
Mau224 <46903680+Mau224@users.noreply.github.com>
2021-02-05 17:20:32 +0300

    fff

--- profilemet-test/js/main.js
+++ profilemet-test/js/main.js
@@ -3729,3 +3729,87 @@ validEmail2.addEventListener('input', function(){
         delete e.default
     }
 });
+
+// //тут скрипт модальных окон которые появляются через обращение к data-modal чаще всего это независимые окна ,которые поялвяются поверх всего
+// let body = document.querySelector('body');
+//
+// !function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
+//
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//
+//     /* Записываем в переменные массив элементов-кнопок и подложку.
+//        Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+//     var modalButtons = document.querySelectorAll('.js-open-modal'),
+//       overlay      = document.querySelector('.js-overlay-modal'),
+//       closeButtons = document.querySelectorAll('.js-modal-close');
+//
+//
+//     /* Перебираем массив кнопок */
+//     modalButtons.forEach(function(item){
+//
+//         /* Назначаем каждой кнопке обработчик клика */
+//         item.addEventListener('click', function(e) {
+//
+//             e.preventDefault();
+//
+//             var modalId = this.getAttribute('data-modal'),
+//               modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+//
+//
+//             /* После того как нашли нужное модальное окно, добавим классы
+//                подложке и окну чтобы показать их. */
+//             modalElem.classList.add('active');
+//             overlay.classList.add('active');
+//             body.classList.add('dis');
+//         }); // end click
+//
+//     }); // end foreach
+//
+//
+//     closeButtons.forEach(function(item){
+//
+//         item.addEventListener('click', function(e) {
+//             var parentModal = this.closest('.modal');
+//
+//             parentModal.classList.remove('active');
+//             overlay.classList.remove('active');
+//             document.querySelector('.open__mega-menu').classList.remove('active');
+//             body.classList.remove('dis');
+//         });
+//
+//     }); // end foreach
+//
+//
+//     document.body.addEventListener('keyup', function (e) {
+//         var key = e.keyCode;
+//
+//         if (key == 27) {
+//
+//             document.querySelector('.modal.active').classList.remove('active');
+//             document.querySelector('.overlay-modal').classList.remove('active');
+//             document.querySelector('.open__mega-menu').classList.remove('active');
+//             document.querySelector('.header').classList.remove('index-max');
+//             document.querySelector('.header').classList.remove('active');
+//             document.querySelector('.text-swap').textContent = 'Mеню';
+//             body.classList.remove('dis');
+//         };
+//     }, false);
+//
+//
+//     overlay.addEventListener('click', function() {
+//         document.querySelector('.modal.active').classList.remove('active');
+//         this.classList.remove('active');
+//         document.querySelector('.open__mega-menu').classList.remove('active');
+//         document.querySelector('.header').classList.remove('index-max');
+//         document.querySelector('.header').classList.remove('active');
+//         document.querySelector('.text-swap').textContent = 'Mеню';
+//         body.classList.remove('dis');
+//     });
+//
+//
+//
+//
+// }); // end ready
+//
+// //
