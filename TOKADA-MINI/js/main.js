7ed414dcde774b2e707b94793c538b3e94363c4d
Mau224 <46903680+Mau224@users.noreply.github.com>
2021-04-04 18:02:47 +0300

    fff

--- TOKADA-MINI/js/main.js
+++ TOKADA-MINI/js/main.js
@@ -939,6 +939,42 @@ document.addEventListener('DOMContentLoaded', () => {
 			secondsVal.style.letterSpacing = '6.75vw';
 		}
 
+		if (window.innerWidth > 1600) {
+			if (hours < 10) {
+				hoursVal.textContent = '0' + hours;
+				hoursVal.style.letterSpacing = '5.8vw';
+			} else if (hours < 20) {
+				hoursVal.textContent = hours;
+				hoursVal.style.letterSpacing = '7.7vw';
+			} else {
+				hoursVal.textContent = hours;
+				hoursVal.style.letterSpacing = '5.8vw';
+			}
+
+			if (minutes < 10) {
+				minutesVal.textContent = '0' + minutes;
+				minutesVal.style.letterSpacing = '5.8vw';
+			} else if (minutes < 20) {
+				minutesVal.textContent = minutes;
+				minutesVal.style.letterSpacing = '6.7vw';
+			} else {
+				minutesVal.textContent = minutes;
+				minutesVal.style.letterSpacing = '5.8vw';
+			}
+
+			if (seconds < 10) {
+				secondsVal.textContent = '0' + seconds;
+				secondsVal.style.letterSpacing = '5.7vw';
+			} else if (seconds < 20) {
+				secondsVal.textContent = seconds;
+				secondsVal.style.letterSpacing = '6.1vw';
+			} else {
+				secondsVal.textContent = seconds;
+				secondsVal.style.letterSpacing = '5.7vw';
+			}
+		}
+
+
 		if (window.innerWidth < 870) {
 			if (hours < 10) {
 				hoursVal.textContent = '0' + hours;
