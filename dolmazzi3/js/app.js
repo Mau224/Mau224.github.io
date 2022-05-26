730fe80fd6c629581527f9b5c1d01f79842da15c
Mau224 <46903680+Mau224@users.noreply.github.com>
2022-05-26 18:18:14 +0300

    new

--- dolmazzi3/js/app.js
+++ dolmazzi3/js/app.js
@@ -13220,8 +13220,8 @@
                 const wrapperPreview = document.querySelector(".wrapper-preview");
                 const prevBlock = document.querySelector(".prev__blok > p");
                 const prevImg = document.querySelector(".prev__img-wrapper");
-                let prevBlockOffset = prevBlock.getBoundingClientRect().top;
-                let prevImgOffset = prevImg.getBoundingClientRect().top;
+                let prevBlockOffset = prevBlock?.getBoundingClientRect()?.top;
+                let prevImgOffset = prevImg?.getBoundingClientRect()?.top;
                 let translateImage = prevImgOffset - prevBlockOffset;
                 window.addEventListener("optimizedResize", (() => {
                     prevBlockOffset = prevBlock.getBoundingClientRect().top;
@@ -13427,6 +13427,43 @@
                 setSliderMax();
             }));
         }
+        gsapWithCSS.registerPlugin(ScrollTrigger_ScrollTrigger);
+        const videoScroll_image = document.querySelector(".video-background");
+        const frameCount = 148;
+        const currentFrame = index => `/img/video/Full_5_1${index.toString().padStart(3, "0")}.jpg`;
+        const videoScroll_preloadImages = () => {
+            for (let i = 0; i <= frameCount; i++) {
+                const img = new Image;
+                img.src = currentFrame(i);
+            }
+        };
+        const img = new Image;
+        img.src = currentFrame(1);
+        img.onload = () => {
+            videoScroll_image.src = img.src;
+        };
+        const updateImage = index => {
+            img.src = currentFrame(index);
+            videoScroll_image.src = img.src;
+        };
+        const scrollVideo = self => {
+            const scrollTop = Math.floor(100 * self.progress);
+            const maxScrollTop = 100;
+            const scrollFraction = scrollTop / maxScrollTop;
+            const frameIndex = Math.min(frameCount - 1, Math.ceil(scrollFraction * frameCount));
+            requestAnimationFrame((() => updateImage(frameIndex + 1)));
+        };
+        gsapWithCSS.timeline({
+            scrollTrigger: {
+                trigger: ".video__wrap",
+                start: () => `top ${(window.innerHeight - document.querySelector(".video-background").clientHeight) / 2}`,
+                end: "bottom bottom",
+                pin: true,
+                scrub: true,
+                onUpdate: scrollVideo
+            }
+        });
+        videoScroll_preloadImages();
         window["FLS"] = true;
         isWebp();
         menuInit();
